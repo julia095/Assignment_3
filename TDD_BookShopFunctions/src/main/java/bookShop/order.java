5ea6c7b912ea3c0059e52adb5181b67cd79290ec
Iuliia Tekin <Iuliia.Tekin@soit.local>
2018-10-02 10:34:15 +1300

    added order class

---
+++ TDD_BookShopFunctions/src/main/java/bookShop/order.java
@@ -0,0 +1,17 @@
+package bookShop;
+
+public class order {
+	
+	
+
+	public static double TotalPrice(Object book1, Object book2, Object book3) {
+		return price;
+	}
+
+	
+	public Double getCount(Book book1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
--- TDD_BookShopFunctions/src/test/java/bookShop/totalPriceTest.java
+++ TDD_BookShopFunctions/src/test/java/bookShop/totalPriceTest.java
@@ -11,9 +11,18 @@ import org.junit.Test;
 
 public class totalPriceTest {
 
+	private Book book1;
+	private Book book2;
+	private Book book3;
+	private order order1;
+	
+	
+	
 	@Before
 	public void setUp() throws Exception {
-		Book book = new Book();
+		Book book1 = new Book(1, "Name1", 20.5);
+		Book book2 = new Book(2, "Name2", 40.5);
+		Book book3 = new Book(3, "Name3", 20.4);
 		
 	}
 
@@ -22,8 +31,11 @@ public class totalPriceTest {
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testTotalPrice() {
+		double expected = book1.Price*order1.getCount(book1)+book2.Price*order1.getCount(book2);
+		double actual = order1.TotalPrice(book1,book2,book3);
+		assertEquals(expected, actual, 0.01);
+		//fail("Not yet implemented");
 	}
 
 }
