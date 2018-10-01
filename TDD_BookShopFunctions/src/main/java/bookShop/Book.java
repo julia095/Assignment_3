7e6b44c99bc82d2200aab8e7f969bf598e775797
Iuliia Tekin <Iuliia.Tekin@soit.local>
2018-10-02 09:43:43 +1300

    first empty test to check total Price, book class

---
+++ TDD_BookShopFunctions/src/main/java/bookShop/Book.java
@@ -0,0 +1,17 @@
+package bookShop;
+
+public class Book {
+	
+	int Number;
+	String Name;
+	Double Price;
+	
+	public Book(int number, String name, Double price) {
+		super();
+		Number = number;
+		Name = name;
+		Price = price;
+	}
+	
+
+}
---
+++ TDD_BookShopFunctions/src/test/java/bookShop/totalPriceTest.java
@@ -0,0 +1,29 @@
+package bookShop;
+
+//Requirements: create a simple totalPrice calculator for the Book Shop
+//
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class totalPriceTest {
+
+	@Before
+	public void setUp() throws Exception {
+		Book book = new Book();
+		
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
